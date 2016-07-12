6945dbc516c3e17850475907e4884d62fced43fc
mathasaiamar <amarhussey@gmail.com>
2016-07-12 17:50:04 +0530

    sum of two numbers

---
+++ Sum.java
@@ -0,0 +1,19 @@
+package corejava;
+
+public class Sum {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	  int z = 10;
+	  float q = 3.21f;
+	  int c;
+	  c = (int) (z + q) ;
+	  System.out.println(c);
+	  
+	  
+	  
+		
+
+	}
+
+}
